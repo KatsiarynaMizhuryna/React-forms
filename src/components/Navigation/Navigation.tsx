239f897afd111c9fcc1633d208ec6ba81c92147f
Katsiaryna Mizhuryna <katsiaryna.mizhuryna@gmail.com>
2024-08-18 11:32:54 +0300

    fix: code formatting

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,30 @@
 import styles from './Navigation.module.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 export default function Navigation() {
+  // const location = useLocation();
+  // const currentPath = location.pathname;
   return (
     <nav className={styles.navigation}>
-      <Link className={styles.link} to="/">Main</Link>
-      <Link className={styles.link} to="/uncontrolled-form">Uncontrolled Form</Link>
-      <Link className={styles.link} to="/controlled-form">Controlled Form</Link>
+      <NavLink 
+        to="/" 
+        className={({ isActive }) => isActive ? styles.activeLink : styles.link}
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/uncontrolled-form" 
+        className={({ isActive }) => isActive ? styles.activeLink : styles.link}
+      >
+        Uncontrolled Form
+      </NavLink>
+      <NavLink 
+        to="/controlled-form" 
+        className={({ isActive }) => isActive ? styles.activeLink : styles.link}
+      >
+        Controlled Form
+      </NavLink>
+      
     </nav>
   );
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import UserData from '../../components/UserData/UserData';
-import styles from './Main.module.css'
+import styles from './Main.module.css';
 
 export default function Main() {
   const UncontrolledFormData = useSelector(
@@ -16,39 +16,39 @@ export default function Main() {
   const lastAddedControlledFormId = useSelector(
     (state: RootState) => state.formSliceReducer.lastAddedControlledFormId
   );
-  
+
   return (
-    <div className={styles.mainContainer}>    
-    <div className={styles.formSection}>
-      <h2 className={styles.sectionTitle}>Uncontrolled Form Data</h2>
-      {UncontrolledFormData && UncontrolledFormData.length > 0 ? (
-        UncontrolledFormData.map((data, index) => (
-          <UserData
-            id={index + 1}
-            formData={data}
-            key={index}
-            isLast={index === lastAddedUncontrolledFormId}
-          />
-        ))
-      ) : (
-        <p className={styles.noData}>No data available</p>
-      )}
+    <div className={styles.mainContainer}>
+      <div className={styles.formSection}>
+        <h2 className={styles.sectionTitle}>Uncontrolled Form Data</h2>
+        {UncontrolledFormData && UncontrolledFormData.length > 0 ? (
+          UncontrolledFormData.map((data, index) => (
+            <UserData
+              id={index + 1}
+              formData={data}
+              key={index}
+              isLast={index === lastAddedUncontrolledFormId}
+            />
+          ))
+        ) : (
+          <p className={styles.noData}>No data available</p>
+        )}
+      </div>
+      <div className={styles.formSection}>
+        <h2 className={styles.sectionTitle}>Controlled Form Data</h2>
+        {ControlledFormData && ControlledFormData.length > 0 ? (
+          ControlledFormData.map((data, index) => (
+            <UserData
+              id={index + 1}
+              formData={data}
+              key={index}
+              isLast={index === lastAddedControlledFormId}
+            />
+          ))
+        ) : (
+          <p className={styles.noData}>No data available</p>
+        )}
+      </div>
     </div>
-    <div className={styles.formSection}>
-      <h2 className={styles.sectionTitle}>Controlled Form Data</h2>
-      {ControlledFormData && ControlledFormData.length > 0 ? (
-        ControlledFormData.map((data, index) => (
-          <UserData
-            id={index + 1}
-            formData={data}
-            key={index}
-            isLast={index === lastAddedControlledFormId}
-          />
-        ))
-      ) : (
-        <p className={styles.noData}>No data available</p>
-      )}
-    </div>
-  </div>
   );
 }
